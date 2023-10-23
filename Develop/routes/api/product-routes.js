e701538fc65826ede9e3c1f46745af1919f87f01
viaheather <viaheatherr@gmail.com>
2023-10-22 20:14:53 -0700

    tag routes added

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -116,6 +116,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const products = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!products) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
